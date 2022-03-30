b660ec17f56e4ca99d0c8bd3f9f78cc8118f434a
shivamguptarke <shivamguptarke@gmail.com>
2022-03-30 12:21:56 +0530

    day 1

---
+++ lib/HomePage.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:state_management_flutter/providers/counter_provider.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    int _counter = Provider.of<Counter>(context).count;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            FloatingActionButton(
+              onPressed: Provider.of<Counter>(context,listen: false).decrement,
+              tooltip: 'Increment',
+              child: const Icon(CupertinoIcons.minus),
+            ),
+            SizedBox(width: 20,),
+            FloatingActionButton(
+              onPressed: Provider.of<Counter>(context,listen: false).reset,
+              tooltip: 'Increment',
+              child: Text(_counter.toString()),
+            ),
+            SizedBox(width: 20,),
+            FloatingActionButton(
+              onPressed: Provider.of<Counter>(context,listen: false).increment,
+              tooltip: 'Increment',
+              child: const Icon(CupertinoIcons.add),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:state_management_flutter/providers/counter_provider.dart';
+import 'HomePage.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context) => Counter()),
+      ],
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: const MyHomePage(title: 'Flutter State Management'),
+      ),
+    );
+  }
+}
